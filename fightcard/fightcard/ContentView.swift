c0c9d819d97ab4a13ee8c426392f6ae6fffd1c24
AD <silentflute.ad@gmail.com>
2020-08-08 16:15:36 +0100

    Initial project build

---
+++ fightcard/fightcard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  fightcard
+//
+//  Created by Amarjit on 08/08/2020.
+//  Copyright © 2020 Amarjit. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
