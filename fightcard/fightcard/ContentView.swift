40d3041d4e9bc3f74856a09d30694aa666cfcf18
AD <silentflute.ad@gmail.com>
2020-08-11 14:59:19 +0100

    Update segment cell for fighter

--- fightcard/fightcard/ContentView.swift
+++ fightcard/fightcard/ContentView.swift
@@ -10,29 +10,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        GeometryReader { geometry in
             VStack {
-                Text("UFC Fight Night").font(.headline)
-                Text("Date: Sun, 2 Aug").font(.caption)
-                Text("Venue: UFC Apex, Las Vegas").font(.caption)
-            }
-            VStack {
-                Text("Middleweight · Main Event")
-                HStack {
-                    AvatarView(imageName: "derek-brunson")
-                    VStack {
-                        Text("Derek Brunson")
-                        Text("21 - 7 - 0").font(.caption)
+                VStack(alignment: .center, spacing: 1.0) {
+                    Text("UFC Fight Night").font(.headline)
+                    Text("Sun, 2 Aug").font(.caption)
+                    Text("Venue: UFC Apex, Las Vegas").font(.caption)
+                }
+                VStack {
+                    Text("Middleweight · Main Event")
+                    HStack(alignment: .center, spacing: 1.0) {
+                        HStack {
+                             Image("derek-brunson")
+                                .resizable()
+                                .frame(maxWidth:60, maxHeight: 60, alignment: .center)
+                                .clipShape(Circle())
+                                    .shadow(radius: 10)
+                                    .overlay(
+                                     Circle().stroke(Color(UIColor(red:135, green: 135, blue: 135, alpha: 1)),
+                                                      lineWidth: 2))
+                            VStack(alignment: .leading, spacing: 2.0) {
+                                Text("Derek Brunson").font(.headline)
+                                Text("21 - 7 - 0").font(.caption)
+                             }
+                         }
                     }
                 }
-                HStack {
-                   AvatarView(imageName: "edmen-shahbazyan")
-                   VStack {
-                       Text("Edmen Shahbazyan")
-                       Text("11 - 1 - 0").font(.caption)
-                   }
-               }
             }
+            .frame(width: geometry.size.width,
+                   alignment: .center)
         }
     }
 }
